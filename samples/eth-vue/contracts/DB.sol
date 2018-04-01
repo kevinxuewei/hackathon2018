0e670afd239108d6579349d553db42dc63c49769
xuewei <mailxuewei@qq.com>
2018-04-02 01:53:10 +0800

    more samples added

--- samples/eth-vue/contracts/DB.sol
+++ samples/eth-vue/contracts/DB.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.18;
+pragma solidity ^0.4.18;
 
 import './zeppelin/lifecycle/Destructible.sol';
 import "./zeppelin/math/SafeMath.sol";
--- samples/eth-vue/truffle.js
+++ samples/eth-vue/truffle.js
@@ -34,9 +34,8 @@ module.exports = {
     },
     development: {
       host: "localhost",
-      port: 8545, // This is the conventional port. If you're using the Ganache Blockchain, change port value to the Ganache default port 7545. If you're using Truffle develop network, change port value to 9545
+      port: 7545, // This is the conventional port. If you're using the Ganache Blockchain, change port value to the Ganache default port 7545. If you're using Truffle develop network, change port value to 9545
       network_id: "*", // Match any network id. You may need to replace * with your network Id
-      from: "", // Add your unlocked account within the double quotes
       gas: 4444444
     }
   }
