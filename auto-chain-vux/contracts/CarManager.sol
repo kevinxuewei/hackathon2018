ee63f6a488c095dff96ae42f5243d593d3f0305f
huangguo <huangguo@xiaomi.com>
2018-04-11 16:11:07 +0800

    火火火火火

---
+++ auto-chain-vux/contracts/CarManager.sol
@@ -0,0 +1,301 @@
+pragma solidity ^0.4.18;
+
+contract CarManager{
+
+  enum CarStatus { RAISING,//募集
+                    FREE, //闲置
+                    BID_USE,//竞价使用
+                    USING, //正在使用
+                    SOLD_OUT_ING //售出
+                  }
+
+  enum TransactionType {
+    BUY,
+    RENT,
+    SOLD_OUT
+  }
+
+  struct User{
+    address add;
+    bytes32 name;
+    int256 property;
+    Car[] havingCars;
+    Car[] usingCars;
+    Transaction[] transactions;
+  }
+
+  struct Location{
+    ufixed longitude;//经度
+    ufixed latitude;//纬度
+    bytes32 name;
+  }
+
+  struct Stock{
+    User user;
+    uint8 percent;
+    uint256 price;
+  }
+
+  struct Car{
+    uint carId;
+    bytes32 name;
+    Location location;
+    uint256 price;
+    bytes32 desc;
+    CarStatus carStatus;
+    bytes32[] imgs;
+    Stock[] stocks;
+    RentInfo[] rentInfos;
+  }
+
+  struct RentInfo{
+    uint256 startTime;
+    uint256 endTime;
+    uint256 price;
+    User user;
+  }
+
+
+  struct Transaction{
+    User user;
+    TransactionType transactionType;
+    uint256 price;
+    uint256 time;
+  }
+
+
+  //用户信息
+  mapping (address => User)  users;
+
+  //汽车信息
+  Car[] allCars;
+
+  //汽车唯一标识
+  uint256 carId = 0;
+
+  //
+  mapping (uint256 => Car) carMap;
+
+
+
+  function returnCar(uint256 _carId,ufixed longitude,ufixed latitude,bytes32 locationName) internal
+  onlyExistingUser
+  onlyExistingCar(_carId)
+  returns (bool){
+    uint curTime = block.timestamp;
+    Car car = carMap[_carId];
+    require(car.carStatus == CarStatus.USING);
+    User user = users[msg.sender];
+    RentInfo[] rentInfos = car.rentInfos;
+    Location memory location = Location(longitude,latitude,locationName);
+    car.location = location;
+    require(user.add == rentInfos[rentInfos.length -1].user.add);
+    Car[] usingCars = user.usingCars;
+    Car[] newUsingCars;
+    for(uint i = 0 ; i < usingCars.length; ++i){
+      if(car.carId != usingCars[i].carId){
+        newUsingCars.push(usingCars[i]);
+      }
+    }
+    user.usingCars = newUsingCars;
+    car.carStatus = CarStatus.FREE;
+    rentInfos[rentInfos.length].endTime = curTime;
+    //TODO 计算价格转账
+    return true;
+  }
+
+
+  function rentNewCar(uint256 _carId,uint256 price) internal
+  onlyExistingUser
+  onlyExistingCar(_carId)
+  returns (bool){
+    uint curTime = block.timestamp;
+    Car car = carMap[_carId];
+    require(car.carStatus == CarStatus.FREE);
+    car.carStatus = CarStatus.USING;
+    User user = users[msg.sender];
+    RentInfo memory rentInfo = RentInfo(curTime,0,price,user);
+    car.rentInfos.push(rentInfo);
+    user.usingCars.push(car);
+    Transaction memory transaction = Transaction(user,TransactionType.RENT,price,curTime);
+    user.transactions.push(transaction);
+    return true;
+  }
+
+
+  function raisingNewCar(bytes32 carName,bytes32 desc,uint price,bytes32[] imgs) internal
+  onlyExistingUser
+  returns (bool){
+    ++carId;
+    Location memory defaultLocation = Location(0,0,"");
+    Car newCar;
+    newCar.carId = carId;
+    newCar.name = carName;
+    newCar.location = defaultLocation;
+    newCar.carStatus = CarStatus.RAISING;
+    newCar.desc = desc;
+    newCar.price = price;
+    newCar.imgs = imgs;
+    carMap[carId] = newCar;
+    allCars.push(newCar);
+    return true;
+  }
+
+
+
+  function buyNewCar(uint256 _carId,uint256 price,uint8 percent) internal
+  onlyExistingUser
+  onlyExistingCar(_carId)
+  returns (bool){
+    uint curTime = block.timestamp;
+    require(!carIsFinishRaise(_carId));
+    Car car = carMap[_carId];
+    Stock[] stocks = car.stocks;
+    uint256 raisedPrice = 0;
+    uint8 raisedPercent = 0;
+    for(uint i = 0 ; i < stocks.length ; ++i){
+      raisedPrice += stocks[i].price;
+      raisedPercent += stocks[i].percent;
+    }
+    require(raisedPrice + price <= car.price);
+    require(raisedPercent + percent <= 100);
+    //转账操作 TODO
+    User user = users[msg.sender];
+    Stock memory newStock = Stock(user,percent,price);
+    car.stocks.push(newStock);
+    user.havingCars.push(car);
+    Transaction memory transaction = Transaction(user,TransactionType.BUY,price,curTime);
+    user.transactions.push(transaction);
+    if(carIsFinishRaise(_carId)){
+      car.carStatus = CarStatus.FREE;
+    }
+
+  }
+
+
+
+  function carIsFinishRaise(uint256 _carId) internal
+  onlyExistingCar(_carId)
+  returns (bool){
+    Car car = carMap[_carId];
+    Stock[] stocks = car.stocks;
+    uint256 raisedPrice = 0;
+    uint8 raisedPercent = 0;
+    for(uint i = 0 ; i < stocks.length ; ++i){
+      raisedPrice += stocks[i].price;
+      raisedPercent += stocks[i].percent;
+    }
+    return raisedPrice == car.price && raisedPercent == 100;
+
+  }
+
+
+
+  function getAllCarsBelongedToUser() internal
+  onlyExistingUser
+  returns(Car[]){
+    User user = users[msg.sender];
+    Car[] cars;
+    for(uint i =0 ; i < user.havingCars.length; ++i){
+      cars.push(user.havingCars[i]);
+    }
+    return cars;
+  }
+
+
+  function getAllCarsUserUsing() internal
+  onlyExistingUser
+  returns (Car[]){
+    User user = users[msg.sender];
+    Car[] cars;
+    for(uint i =0 ; i < user.usingCars.length; ++i){
+      cars.push(user.usingCars[i]);
+    }
+    return cars;
+  }
+
+
+  function getCarDetail(uint256 _carId) internal
+  onlyExistingUser
+  onlyExistingCar(_carId)
+  returns (Car){
+    return carMap[_carId];
+  }
+
+
+
+  function getAllCars() internal
+  onlyExistingUser
+  returns (Car[]){
+    return allCars;
+  }
+
+
+  function getCarListByStatus(CarStatus carStatus) internal
+  onlyExistingUser
+  returns (Car[]){
+    Car[] res;
+    for(uint i = 0; i < allCars.length; i++) {
+      if(allCars[i].carStatus == carStatus){
+        res.push(allCars[i]);
+      }
+    }
+    return res;
+  }
+
+
+  function getUserDetail() internal
+  onlyExistingUser
+  returns (User){
+    return users[msg.sender];
+  }
+
+
+  modifier onlyExistingCar (uint256 _carId){
+    require(!(carMap[_carId].name == 0x0));
+    _;
+  }
+
+
+  modifier onlyExistingUser {
+    require(!(users[msg.sender].name == 0x0));
+    _;
+  }
+
+  modifier onlyValidName(bytes32 name) {
+    require(!(name == 0x0));
+    _;
+  }
+
+  function login() constant public
+  onlyExistingUser
+  returns (bytes32) {
+    return (users[msg.sender].name);
+  }
+
+  function signup(bytes32 name) public payable
+  onlyValidName(name)
+  returns (bytes32) {
+    if (users[msg.sender].name == 0x0) {
+      users[msg.sender].name = name;
+      users[msg.sender].add = msg.sender;
+      return (users[msg.sender].name);
+    }
+    return (users[msg.sender].name);
+  }
+
+  function update(bytes32 name) public payable
+  onlyValidName(name)
+  onlyExistingUser
+  returns (bytes32) {
+
+    if (users[msg.sender].name != 0x0)
+    {
+      users[msg.sender].name = name;
+
+      return (users[msg.sender].name);
+    }
+  }
+
+
+}
--- auto-chain-vux/src/main.js
+++ auto-chain-vux/src/main.js
@@ -10,6 +10,7 @@ import VueRouter from 'vue-router'
 import { sync } from 'vuex-router-sync'
 import vuxLocales from './locales/all.yml'
 import componentsLocales from './locales/components.yml'
+import Web3 from 'web3'
 
 Vue.use(VueRouter)
 Vue.use(Vuex)
@@ -203,8 +204,27 @@ router.afterEach(function (to) {
   }
 })
 
-new Vue({
-  store,
-  router,
-  render: h => h(App)
-}).$mount('#app')
+window.addEventListener('load', function () {
+  if (typeof web3 !== 'undefined') {
+    console.log('Web3 injected browser: OK.')
+    window.web3 = new Web3(window.web3.currentProvider)
+  } else {
+    console.log('Web3 injected browser: Fail. You should consider trying MetaMask.')
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'))
+  }
+
+  /* eslint-disable no-new */
+  new Vue({
+    store,
+    router,
+    render: h => h(App)
+  }).$mount('#app')
+  // new Vue({
+  //   el: '#app',
+  //   router,
+  //   components: { App },
+  //   template: '<App/>'
+  // })
+})
+
