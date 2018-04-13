509b97d4231a1b1ec8a05fc5eef97a5e7c2965c5
zhonghcc <zhonghcc@gmail.com>
2018-04-14 04:18:05 +0800

    update collect

--- auto-chain/src/components/Collect.vue
+++ auto-chain/src/components/Collect.vue
@@ -5,9 +5,10 @@
     <div slot="content" class="card-padding">
       <group :title="'费用单位为 代币/小时'">
         <x-input title="车辆名称：" :max="10" v-model="carName"></x-input>
-        <x-input title="总金额：" v-model="amount"></x-input>
-        <x-input title="发行代币：" v-model="soldAmount"></x-input>
-        <x-input title="费用：" v-model="price"></x-input>
+        <x-input title="总金额(WEI)：" v-model="price"></x-input>
+        <x-input title="总代币：" v-model="amount"></x-input>
+        <x-input title="募集代币：" v-model="soldAmount"></x-input>
+        <x-input title="用车费用(小时)：" v-model="rentAmount"></x-input>
       </group>
       <box gap="20px 20px">
         <x-button :gradients="['#1D62F0', '#19D5FD']" @click.native="submit">发起募集</x-button>
@@ -32,6 +33,7 @@
         carName: "",
         amount: "",
         soldAmount: "",
+        rentAmount: "",
         desc: "",
         imgs: "",
         price: "",
@@ -49,20 +51,30 @@
         } else if (this.amount == '') {
           AlertModule.show({
             title: '错误',
-            content: '请输入总金额'
+            content: '请输入总代币'
+          })
+        } else if (this.rentAmount == '') {
+          AlertModule.show({
+            title: '错误',
+            content: '请输入用车费用'
           })
         } else if (this.soldAmount == '') {
           AlertModule.show({
             title: '错误',
-            content: '请输入发行代币'
+            content: '请输入要募集的代币'
           })
         } else if (this.price == '') {
           AlertModule.show({
             title: '错误',
-            content: '请输入费用'
+            content: '请输入ETH价格'
+          })
+        } else if (parseInt(this.soldAmount) > parseInt(this.amount)) {
+          AlertModule.show({
+            title: '错误',
+            content: '出让代币不可大于总代币'
           })
         } else {
-          window.CarManager.raisingNewCar(this.carName, this.desc, this.price, this.amount, this.imgs, this.soldAmount).then(username => {
+          window.CarManager.raisingNewCar(this.carName, this.desc, this.price, this.amount, this.imgs, this.soldAmount, this.rentAmount).then(username => {
             self.$router.push('/success')
           })
         }
